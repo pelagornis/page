bf88c122b9900665412e566e0cdf16f3c2ee1356
Chilensis <chilensis.dev@gmail.com>
2025-02-21 02:11:37 +0900

    Update Package.json

--- examples/basics/src/content.config.ts
+++ examples/basics/src/content.config.ts
@@ -1,6 +1,6 @@
 import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader } from '@pelagornis/page/loaders';
+import { docsSchema } from '@pelagornis/page/schema';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
